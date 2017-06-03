57c525c06931416164c0bdb89615faded12a8b7c
John Ballon <johnballon@Johns-MacBook-Pro.local>
2017-06-03 11:24:43 -0700

    Pascal Python

--- python_practice.py
+++ python_practice.py
@@ -14,3 +14,20 @@ def unique_items(arr):
 
 
 unique_items([1,1,1,3,4,4,6])
+
+def pascals_triangle(n):
+    triangle = [[1]]
+    while len(triangle) < n + 1:
+      triangle.append(next_row(triangle[len(triangle) - 1]))
+    print triangle
+
+def next_row(last_row):
+  row = [1]
+  i = 0
+  while i < len(last_row) - 1:
+    row.append(sum([last_row[i], last_row[i + 1]]))
+    i += 1
+  row.append(1)
+  return row
+
+pascals_triangle(2)
