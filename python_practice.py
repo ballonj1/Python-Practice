63f763f56bc4f6457db2bb0ac97fc16dd6401489
John Ballon <johnballon@Johns-MacBook-Pro.local>
2017-06-03 01:37:34 -0700

    unique_items

--- python_practice.py
+++ python_practice.py
@@ -3,3 +3,14 @@ def mult_by_two(arr):
         print num * 2
 
 mult_by_two([1,2,3,4])
+
+
+def unique_items(arr):
+  unique = []
+  for num in arr:
+      if arr.count(num) == 1:
+        unique.append(num)
+  print unique
+
+
+unique_items([1,1,1,3,4,4,6])
